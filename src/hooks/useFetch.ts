4ce5addab3f2cba70c5cf0d6b63f02eef5699381
Matheus Souza <t.matheus.souza@self.inc>
2021-11-10 15:17:59 -0300

    Create useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,36 @@
+import {useState, useCallback} from 'react';
+
+type ReturnType<T> = [
+  {data: T | null; loading: boolean; error: boolean},
+  () => Promise<void>,
+];
+
+const useFetch = <TData>(url: string): ReturnType<TData> => {
+  const [data, setData] = useState<TData | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const beforeFetch = () => {
+    setData(null);
+    setError(false);
+    setLoading(true);
+  };
+
+  const fetchData = useCallback(async () => {
+    beforeFetch();
+
+    try {
+      const res = await fetch(url);
+      const parsedData = await res.json();
+      setData(parsedData);
+      setLoading(false);
+    } catch {
+      setError(true);
+      setLoading(false);
+    }
+  }, [url]);
+
+  return [{data, loading, error}, fetchData];
+};
+
+export default useFetch;
